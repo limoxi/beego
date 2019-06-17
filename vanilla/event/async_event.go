addb278b422bd0cf425e54d0237910125fa3e308
limoxi <asia-aixiang@163.com>
2019-06-17 10:17:12 +0800

    'fix'

--- vanilla/event/async_event.go
+++ vanilla/event/async_event.go
@@ -12,10 +12,13 @@ type asyncEvent struct{}
 
 func (ae *asyncEvent) Send(event *Event, data map[string]interface{}){
 	data["_time"] = time.Now().Format("2006-01-02 15:04:05")
-	data["_event_name"] = event.Name
+	messageData := map[string]interface{}{
+		"_event_name": event.Name,
+		"data": data,
+	}
 	engineType := beego.AppConfig.String("event::ASYNC_EVENT_ENGINE")
 	if validEngine, ok := engine.Type2Engine[engineType]; ok{
-		validEngine.Send(data, event.Tag)
+		validEngine.Send(messageData, event.Tag)
 	}else{
 		fmt.Printf("[Event] NO ENGINE FOUND")
 	}
